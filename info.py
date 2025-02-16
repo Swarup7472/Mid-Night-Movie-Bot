0688126416bc28294971844a0eec42e033255478
Swarup7472 <swarupdas7472@gmail.com>
2025-02-16 09:12:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', "https://t.me/askmeyaaar") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002382760272'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/AtoZfilmss')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002283911005').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002460751147').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://midnight7472:midnight7472@midnight.m4wsr.mongodb.net/?retryWrites=true&w=majority&appName=Midnight")
